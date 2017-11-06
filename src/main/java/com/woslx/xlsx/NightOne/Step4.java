7d36b10742fc4e13b61e1e66d274ff18c24c7ed6
huangyue <huangyue306@gmail.com>
2017-11-06 17:02:59 +0800

    save

--- src/main/java/com/woslx/xlsx/NightOne/Step4.java
+++ src/main/java/com/woslx/xlsx/NightOne/Step4.java
@@ -1,7 +1,19 @@
 package com.woslx.xlsx.NightOne;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Step4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InvalidFormatException {
+        File ding = new File("/home/hy/tmp/nightone/dingout.xlsx");
+        File yunnan = new File("/home/hy/tmp/nightone/yunnanout.xlsx");
+        File shanghai = new File("/home/hy/tmp/nightone/shanghaiout.xlsx");
+
+        Workbook wbding = new XSSFWorkbook(ding);
 
     }
 }
